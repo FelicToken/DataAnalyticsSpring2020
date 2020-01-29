58490eba2f66ed39a949a8ca0d632c8f3b90e3de
FelicToken <47868673+FelicToken@users.noreply.github.com>
2020-01-29 17:24:25 -0500

    Update lab1.r

--- lab1.r
+++ lab1.r
@@ -1,10 +1,9 @@
 
-data1 <-read.csv(file.choose(),header=TRUE)
+data1 <-read.csv(file.choose(),skip=1)
 attach(data1)
-fix(data1) 
-EPI
-tf <- is.na(EPI) 
-E <- EPI[!tf]
+DALY
+tf <- is.na(DALY) 
+E <- DALY[!tf]
 
 
 summary(DALY)
@@ -16,9 +15,9 @@ lines(density(DALY,na.rm=TRUE,bw=1.))
 rug(DALY) 
 
 
-plot(ecdf(EPI), do.points=FALSE, verticals=TRUE) 
+plot(ecdf(DALY), do.points=FALSE, verticals=TRUE) 
 par(pty="s") 
-qqnorm(EPI); qqline(EPI)
+qqnorm(DALY); qqline(DALY)
 
 x<-seq(30,95,1)
 qqplot(qt(ppoints(250), df = 5), x, xlab = "Q-Q plot for t dsn")
@@ -26,4 +25,31 @@ qqline(x)
 
 
 boxplot(EPI,DALY) 
+
+#try to exploring the WATER_H
+WATER_H
+tf <- is.na(WATER_H) 
+E <- WATER_H[!tf]
+
+
+summary(WATER_H)
+fivenum(WATER_H,na.rm=TRUE)
+stem(WATER_H)
+hist(WATER_H)
+hist(WATER_H, seq(0., 100., 1.0), prob=TRUE)
+lines(density(WATER_H,na.rm=TRUE,bw=1.))
+rug(WATER_H) 
+
+
+plot(ecdf(WATER_H), do.points=FALSE, verticals=TRUE) 
+par(pty="s") 
+qqnorm(WATER_H); qqline(WATER_H)
+
+x<-seq(30,95,1)
+qqplot(qt(ppoints(250), df = 5), x, xlab = "Q-Q plot for t dsn")
+qqline(x)
+
+
+boxplot(EPI,WATER_H) 
+
 
